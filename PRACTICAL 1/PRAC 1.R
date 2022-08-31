7d6fb302c5ab99e9e52839cdebc24cb973e82c40
vidyaj99 <90394585+vidyaj99@users.noreply.github.com>
2022-09-01 00:33:15 +0530

    Add files via upload

---
+++ PRACTICAL 1/PRAC 1.R
@@ -0,0 +1,33 @@
+require(lpSolve)
+
+C <- c(3,5)
+A <- matrix(c(1,2,1,1,0,1), nrow=3, byrow=TRUE)
+
+B <- c(2000, 1500, 600)
+
+constraints_direction <- c("<=","<=","<=")
+
+plot.new()
+plot.window(xlim=c(0,2000), ylim=c(0,2000))
+axis(1)
+axis(2)
+title(main="LPP using graphical method")
+title(xlab="X axis")
+title(ylab="Y axis")
+box()
+
+segments(x0=2000, y0=0, x1=0, y1=1000, col="green")
+segments(x0=1500, y0=0, x1=0, y1=1000, col="green")
+segments(x0=2000, y0=0, x1=600, y1=0, col="green")
+
+optimum <- lp(direction="max", objective.in=C,
+              const.mat=A, const.dir=constraints_direction,
+              const.rhs=B,
+              all.int=T)
+
+print(optimum$status)
+
+best_sol <- optimum$solution
+names(best_sol) <- c("x1","x2")
+print(best_sol)
+print(paste("Total cost:", optimum$objval, sep = ""))
---
+++ PRACTICAL 9/PRAC 9.R
@@ -0,0 +1,19 @@
+library(lpSolve)
+
+costs <- matrix(c(10,2,20,11,
+                  12,7,9,20,
+                  4,14,16,18), nrow = 3, byrow = TRUE)
+
+colnames(costs) <- c("Customer 1","Customer 2","Customer 3","Customer 4")
+rownames(costs) <- c("Supplier 1","Supplier 2","Supplier 3")
+
+row.signs <- rep("<=",3)
+row.rhs <- c(15,25,10)
+
+col.signs <- rep(">=",4)
+col.rhs <- c(5,15,15,15)
+
+TotalCost <- lp.transport(costs,"min",row.signs,row.rhs,col.signs,col.rhs)
+
+lp.transport(costs,"min",row.signs,row.rhs,col.signs,col.rhs)$solution
+print(TotalCost)
